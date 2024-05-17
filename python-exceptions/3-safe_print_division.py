c2058df4ffeff51f4b25ef06c485c99f9402e168
fatmaGma <8891@holbetonstudents.com>
2024-05-17 09:38:29 +0000

    fixing the code for the checker

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -9,7 +9,6 @@ def safe_print_division(a, b):
     Returns:
     float: The result of the division, or None if division by zero.
     """
-    result = None
     try:
         result = a / b
     except ZeroDivisionError:
