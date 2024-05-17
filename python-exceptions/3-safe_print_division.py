20a6aaba761fcbfaa1728642ecdc868c3fe7e478
fatmaGma <8891@holbetonstudents.com>
2024-05-17 09:35:22 +0000

    code + compile + test

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,19 @@
+def safe_print_division(a, b):
+    """
+    Divides two integers and prints the result.
+
+    Args:
+    a (int): The numerator.
+    b (int): The denominator.
+
+    Returns:
+    float: The result of the division, or None if division by zero.
+    """
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
