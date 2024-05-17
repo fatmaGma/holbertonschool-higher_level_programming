9876ff4b564717a0257b7e76010ffe917f4ba690
fatmaGma <8891@holbetonstudents.com>
2024-05-17 11:17:04 +0000

    change the code + test

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """
-Square class defines a square with a private instance attribute: size
+define a class
 """
 class Square:
     def __init__(self, size=0):
         """
-        Initializes a new instance of the Square class with an optional size.
+        Initializes a new square
+        Args:
+        size (int): size of a new square
         """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
