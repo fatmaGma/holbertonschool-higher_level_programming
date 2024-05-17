a50ecdf8f4198ab2644237ef98e17643dfe25823
fatmaGma <8891@holbetonstudents.com>
2024-05-17 10:59:03 +0000

    code + compile + test

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Square class defines a square with a private instance attribute: size
+    """
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class with a given size.
+        """
+        self.__size = size
