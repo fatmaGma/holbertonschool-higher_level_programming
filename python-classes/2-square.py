971e433fd99e37847175d7607660cef572337091
fatmaGma <8891@holbetonstudents.com>
2024-05-17 11:32:36 +0000

    last try

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
 """
-define a class
+Define a square class
 """
+
 class Square:
+    """
+    define attribute of square
+    """
+
     def __init__(self, size=0):
         """
-        Initializes a new square
+        initialize a square
+        
         Args:
-        size (int): size of a new square
+        size (int, optional): _description_. Defaults to 0.
+    
+        Raises:
+        TypeError: size must be an integer
+        ValueError: size must be >=0
         """
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
