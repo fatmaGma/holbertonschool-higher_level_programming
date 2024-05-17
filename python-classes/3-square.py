3c953db237cf9af427f66c5f39c8fc050bf9974d
fatmaGma <8891@holbetonstudents.com>
2024-05-17 11:24:17 +0000

    change the code + test

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Square class defines a square with a private instance attribute: size
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class with an optional size.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Returns the current area of the square.
+        """
+        return self.__size ** 2
