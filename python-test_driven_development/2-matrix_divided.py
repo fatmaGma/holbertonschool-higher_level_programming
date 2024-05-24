4223f3f3ca4df1da87724460515f9b079ea601a9
fatmaGma <8891@holbertonstudents.com>
2024-05-24 10:37:20 +0000

    code + compile + test

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -18,34 +18,25 @@ def matrix_divided(matrix, div):
         TypeError: if div is neither int nor float
         ZeroDivisionError: when div is zero
     """
-    if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists)\
-                of integers/floats")
+    matrix_reloaded = []
+    row_length = 0
+    list_error = "matrix must be a matrix (list of lists) of integers/floats"
 
-    row_size = len(matrix[0])
-    if not all(len(row) == row_size for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-
+    if not (isinstance(matrix, list) or isinstance(matrix[0], list)
+            or len(matrix[0]) <= 0):
+        raise TypeError(list_error)
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-
-    new_matrix = []
-
     for row in matrix:
         new_row = []
-        for element in row:
-            if not isinstance(element, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists)\
-                        of integers/floats")
-            new_element = round(element / div, 2)
-            new_row.append(new_element)
-        new_matrix.append(new_row)
-
-    return new_matrix
-
-    if __name__ == "__main__":
-        import doctest
-        doctest.testfile("tests/2-matrix_divided.txt")
+        if row_length == 0:
+            row_length = len(row)
+        elif not (len(row) == row_length):
+            raise TypeError("Each row of the matrix must have the same size")
+        for num in row:
+            if not isinstance(num, (int, float)):
+                raise TypeError(list_error)
+            new_row.append(round(num / div, 2))
+        matrix_reloaded.append(new_row)
+    return matrix_reloaded
