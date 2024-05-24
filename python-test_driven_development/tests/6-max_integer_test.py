bd81a5ab6334c2ac0368749b7f425ac9c6a02535
fatmaGma <8891@holbertonstudents.com>
2024-05-24 10:50:44 +0000

    correct the code

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,15 +5,32 @@ Unittest for max_integer([..])
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-
 class TestMaxInteger(unittest.TestCase):
-    def test_max_integer(self):
-        my_list = [1, 2, 3, 4]
-        self.assertEqual(max_integer(my_list), 4)
 
-    def test_max_integer(self):
-        my_list = [1, 3, 4, 2]
-        self.assertEqual(max_integer(my_list), 4)
+    def test_max_at_end(self):
+        max_at_end = [1, 2, 3, 4]
+        self.assertAlmostEqual(max_integer(max_at_end), 4)
 
-    def test_empty_list(self):
-        self.assertEqual(max_integer([]), None)
+    def test_max_at_beginning(self):
+        max_at_beginning = [4, 3, 2, 1]
+        self.assertAlmostEqual(max_integer(max_at_beginning), 4)
+
+    def test_max_in_middle(self):
+        malcolm_in_the_middle = [1, 4, 5, 2, 3]
+        self.assertAlmostEqual(max_integer(malcolm_in_the_middle), 5)
+
+    def test_one_negative(self):
+        one_negative = [1, 2, -3, 4]
+        self.assertAlmostEqual(max_integer(one_negative), 4)
+
+    def test_all_negative(self):
+        all_negative = [-1, -2, -3, -4]
+        self.assertAlmostEqual(max_integer(all_negative), -1)
+
+    def test_one_in_list(self):
+        one_in_list = [4]
+        self.assertAlmostEqual(max_integer(one_in_list), 4)
+    
+    def test_list_empty(self):
+        empty = []
+        self.assertAlmostEqual(max_integer(empty), None)
