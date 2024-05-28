9709874468632045d2ca3005769c7ff29fb87f9a
fatmaGma <8891@holbertonstudents.com>
2024-05-28 10:10:05 +0000

    creation of the file tests and removing the files txt in it

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#/usr/bin/python3
+"""module for file appending operations"""
+
+
+def append_write(filename="", text=""):
+    """
+    Append a str to the end of a txt file and retrun the number of char added
+
+    Args:
+    filename (str): the path of the file to be written
+    text (str): the text to be appended to the file
+
+    Returns:
+    int: the number of char added
+    """
+    with open(filename, 'a', encoding='utf-8') as file:
+            return file.write(text)
