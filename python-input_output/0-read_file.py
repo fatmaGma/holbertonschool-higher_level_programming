415277c3217719a4c0af4cffeaf27a16b3686b64
fatmaGma <8891@holbertonstudents.com>
2024-05-28 09:46:32 +0000

    creat the files + code + compile + code

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for file reading operations."""
+
+
+def read_file(filename = ""):
+    """read a file and print it .
+
+    args:
+    filename: file to read and print.
+    """
+    with open(filename, 'r', encoding = 'utf-8') as file:
+        content = file.read()
+        print(content, end'')
+
