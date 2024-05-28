7ac18319d2a6772c7b2af9e4d51cfbad5da06ed7
fatmaGma <8891@holbertonstudents.com>
2024-05-28 09:49:53 +0000

    creat the files + code + compile + code

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -10,5 +10,4 @@ def read_file(filename = ""):
     """
     with open(filename, 'r', encoding = 'utf-8') as file:
         content = file.read()
-        print(content, end'')
-
+        print(content, end='')
