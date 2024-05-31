49a104ac1bdce3722dd3d8857ddfaa4bc7e289ec
fatmaGma <8891@holbertonstudents.com>
2024-05-31 10:46:58 +0000

    code + compile + test

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+function file for add_item.json
+"""
+
+import sys
+from os.path import exists
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+def main():
+    if not exists("add_item.json"):
+        save_to_json_file([], "add_item.json")
+
+    my_list = load_from_json_file("add_item.json")
+
+    # Add command-line arguments to the list
+    my_list.extend(sys.argv[1:])
+
+    # Save the updated list to the file
+    save_to_json_file(my_list, "add_item.json")
+
+if __name__ == "__main__":
+    main()
