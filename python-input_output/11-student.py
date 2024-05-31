00d5c2c640abc6a8656e42ad87ecbbd9ebbea023
fatmaGma <8891@holbertonstudents.com>
2024-05-31 11:42:11 +0000

    test 2

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -4,15 +4,14 @@
 
 class Student:
     """Define a student"""
-
     def __init__(self, first_name, last_name, age):
-    """
-    args:
-    first_name, last_name, et  age
-    """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+        """
+        args:
+        first_name, last_name, et  age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
         if attrs is None:
