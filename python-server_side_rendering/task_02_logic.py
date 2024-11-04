f3683698de1b4f1849a3a3fdd7b8685aa4d01be9
fatmaagmat <8891@holbertonstudents.com>
2024-11-04 08:08:27 +0000

    test*

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import json
 
 app = Flask(__name__)
 
-# Route to render items.html with items from items.json
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/items')
 def items():
-    with open('items.json', 'r') as f:
+    with open('items.json') as f:
         data = json.load(f)
-        items_list = data['items']
-    return render_template('items.html', items=items_list)
+    items = data.get("items", [])
+    return render_template('items.html', items=items)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import json
 import csv
+import sqlite3
 
 app = Flask(__name__)
 
@@ -18,6 +19,23 @@ def read_csv_file(filepath):
             products.append(row)
     return products
 
+def read_sqlite_db():
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Products')
+    rows = cursor.fetchall()
+    conn.close()
+    products = []
+    for row in rows:
+        product = {
+                'id': row[0],
+                'name': row[1],
+                'category': row[2],
+                'price': row[3]
+                }
+        products.append(product)
+    return products
+
 @app.route('/products')
 def products():
     source = request.args.get('source')
@@ -27,6 +45,8 @@ def products():
         products = read_json_file('products.json')
     elif source == 'csv':
         products = read_csv_file('products.csv')
+    elif source == 'sql':
+        products = read_sqlite_db()
     else:
         return render_template('product_display.html', error="Wrong source")
 
@@ -38,4 +58,4 @@ def products():
     return render_template('product_display.html', products=products)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+app.run(debug=True, port=5000)i
